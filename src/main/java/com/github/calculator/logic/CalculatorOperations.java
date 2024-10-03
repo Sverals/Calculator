c576589ac3c539f96e30d5e17e386fdca88c215f
Sverals <44512019+Sverals@users.noreply.github.com>
2024-10-03 16:58:05 -0400

    Setup add functionality
    add method will now add two numbers together and return a double

---
+++ src/main/java/com/github/calculator/logic/CalculatorOperations.java
@@ -0,0 +1,13 @@
+package com.github.calculator.logic;
+
+public class CalculatorOperations {
+
+    public CalculatorOperations() {
+
+    }
+
+    public double add(double x, double y) {
+        return x + y;
+    }
+
+}
